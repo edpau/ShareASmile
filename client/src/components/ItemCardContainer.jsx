6809da0717b751c211f32636ea91246fabf8957a
edpau <edpau@me.com>
2023-09-21 08:21:56 +0100

    feat: Add new components: ItemCardContainer

---
+++ client/src/components/ItemCardContainer.jsx
@@ -0,0 +1,10 @@
+import ItemCard from './ItemCard'
+
+export default function ItemCardContainer() {
+  return (
+    <div className="ItemCardContainer">
+      <p>ItemCardContainer</p>
+      <ItemCard />
+    </div>
+  )
+}
