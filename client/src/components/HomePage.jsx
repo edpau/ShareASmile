6a2ea4c35f5e560e22108fb2613feacb52da6166
edpau <edpau@me.com>
2023-09-21 11:05:51 +0100

    feat: home page has all the componets insert

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,12 +1,14 @@
 import MainHeader from './MainHeader'
-import ItemCardContainer from './ItemCardContainer'
 
+import ItemCardContainer from './ItemCardContainer'
+import CategoryCircle from './CategoryCircle'
 
 export default function HomePage() {
   return (
     <div className="HomePage">
         <MainHeader heading = 'ShareASmile'></MainHeader>
         <input className="searchBar" label="Search"/>
+        <CategoryCircle></CategoryCircle>
         <ItemCardContainer></ItemCardContainer>
     </div>
   )
